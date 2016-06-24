fa8dfc544d3710949e4d28a710f0e5a5a71f5c97
Kevin Lacker <lacker@gmail.com>
2016-06-23 21:14:19 -0700

    full screen should be pressy

--- index.ios.js
+++ index.ios.js
@@ -7,24 +7,41 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  StyleSheet,
   Text,
+  TouchableHighlight,
   View,
 } from 'react-native';
 
 class Wittgenstein extends Component {
+  press() {
+    console.log('press');
+  }
+
   render() {
     return (
-      <View>
-        <Text>
-        Say something. Like "taco"
+      <TouchableHighlight
+        style={styles.container}
+        onPress={() => this.press()}>
+        <Text style={styles.text}>
+          Say something. Like "taco"
         </Text>
-      </View>
+      </TouchableHighlight>
     );
   }
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
 });
 
 AppRegistry.registerComponent('Wittgenstein', () => Wittgenstein);
