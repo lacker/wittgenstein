77cc0011e129fa78f44e26e20e64a2081d8b3004
Kevin Lacker <lacker@gmail.com>
2016-06-26 14:31:00 -0700

    work on the parser

--- data/scrape.js
+++ data/scrape.js
@@ -1,9 +1,34 @@
 // A scraper to gather emoji data
-import fetch from 'node-fetch';
+const fetch = require('isomorphic-fetch');
 
 const url = 'http://unicode.org/emoji/charts/full-emoji-list.html';
 
 
-fetch(url, response => {
-  console.log(response);
+fetch(url).then(response => response.text()).then(body => {
+  const lines = body.split('\n');
+  let filtered = [];
+  let buffer = {};
+
+  function flushBuffer() {
+    if (buffer.emoji) {
+      filtered.push(buffer);
+    }
+    buffer = {};
+  };
+
+  for (let line of lines) {
+    if (line.match(/class=.chars/)) {
+      flushBuffer();
+      buffer.emoji = line.split('>')[1].split('<')[0];
+    } else if (line.match(/class=.name/)) {
+      // TODO: parse smartly
+      filtered.push(line);
+    }
+  }
+  flushBuffer();
+
+  console.log(filtered);
+  console.log(body.length, 'bytes in the body');
 });
+
+console.log('scraping...');
