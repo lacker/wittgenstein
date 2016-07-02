95e528594aa8865ed4938bc9a2808e180f88584a
Kevin Lacker <lacker@gmail.com>
2016-07-01 19:58:36 -0700

    index the terms in the name

--- data/search.js
+++ data/search.js
@@ -7,8 +7,17 @@ let byName = {};
 
 let byKeyword = {};
 
+// Maps each term to a list of emoji that have it in their name
+let byNameTerm = {};
+
 for (let entry of data) {
   byName[entry.name] = entry.emoji;
+  for (let nameTerm of entry.name.split(' ')) {
+    if (!byNameTerm[nameTerm]) {
+      byNameTerm[nameTerm] = [];
+    }
+    byNameTerm[nameTerm].push(entry.emoji);
+  }
   for (let keyword of entry.keywords) {
     if (!byKeyword[keyword]) {
       byKeyword[keyword] = [];
